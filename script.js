5f13088f2acd0b80fc608f64e09c175e6121fe69
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-09-20 17:31:54 -0500

    async await

--- script.js
+++ script.js
@@ -5,16 +5,35 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+// ASYNC/AWAIT
+async function generateJoke() {
     const config = {
         headers: {
             'Accept': 'application/json'
         }
     }
 
-    fetch('https://icanhazdadjoke.com', config)
-    .then(response => response.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    })
+    const response = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await response.json()
+
+    jokeEl.innerHTML = data.joke
+
 }
+
+    
+// USING .then()
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then(response => response.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }
+
